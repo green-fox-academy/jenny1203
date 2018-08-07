900989c92f9319018f850bf55475efe01743d3bc
Jenny <lottj440@gmail.com>
2018-08-07 14:15:56 +0200

    wrong url change

---
+++ week-02/day-02/url-fixer.ts
@@ -0,0 +1,9 @@
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+let newUrl1: string = url.replace('bots', 'odds');
+console.log(newUrl1);
+
+
