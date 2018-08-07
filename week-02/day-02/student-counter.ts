cd86523830320a16261def91fbe409404460a0ee
Jenny <lottj440@gmail.com>
2018-08-07 17:30:24 +0200

    candie-counter+age

---
+++ week-02/day-02/student-counter.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+const students: any[] = [
+    { name: 'Theodor', age: 3, candies: 2 },
+    { name: 'Paul', age: 9.5, candies: 2 },
+    { name: 'Mark', age: 12, candies: 5 },
+    { name: 'Peter', age: 7, candies: 3 },
+    { name: 'Olaf', age: 12, candies: 7 },
+    { name: 'George', age: 10, candies: 1 }
+];
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
+
+function listOfCandi(objects: object[]) {
+    let sum: number = 0;
+    objects.forEach(function (elem) {
+        sum += elem['candies']
+    }); return sum;
+
+}
+
+function lessThanCandi(objects: object[]) {
+    let age: number = 0;
+    objects.forEach(function (elem) {
+        if (elem['candies'] < 5) {
+            age += elem['age'];
+        }
+    }); return age;
+}
+
+console.log(lessThanCandi(students));
+
+console.log(listOfCandi(students));
+
