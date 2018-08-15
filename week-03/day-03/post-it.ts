7c2c198a3a6815e09a5ce0a5e7e13271034df574
Jenny <lottj440@gmail.com>
2018-08-15 21:42:53 +0200

    added post-it

---
+++ week-03/day-03/post-it.ts
@@ -0,0 +1,13 @@
+class postIt {
+   backgroundColor: string;
+    text: string;
+    textColor: string;
+    public constructor(backgroundColor: string, text: string, tectColor: string){
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = this.textColor;
+    }
+}
+let pelda1: postIt = new postIt('orange', 'blue', "blue");
+let pelda2: postIt = new postIt('pink','black','black');
+let pelda3: postIt = new postIt('yellow', 'green', 'green');
