454b4dba5ec96be69f34a84f8e7943aeccaddf97
Jenny <lottj440@gmail.com>
2018-09-07 21:46:19 +0200

    new Carrier class

---
+++ week-05/exm-pract/aircraft/carrier.ts
@@ -0,0 +1,15 @@
+class Carrier {
+  aircrafts: Aircraft[];
+  ammo: number;
+  healthPoint: number;
+
+  constructor(ammo, healthPoint){
+    this.ammo = ammo;
+    this.healthPoint = healthPoint;
+    this.aircrafts = [];
+  }
+
+  add(ammo, baseDamage, maxAmmo, type, allDamage){
+    this.aircrafts.push(new Aircraft(ammo, baseDamage, maxAmmo, type, allDamage));
+  }
+}
