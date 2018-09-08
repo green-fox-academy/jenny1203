fe913942e33c82b9387add925ea744466c5803d5
Jenny <lottj440@gmail.com>
2018-09-08 08:25:14 +0200

    carrier with fill and prioity counter

--- week-05/exm-pract/aircraft/carrier.ts
+++ week-05/exm-pract/aircraft/carrier.ts
@@ -4,22 +4,37 @@ class Carrier {
   aircrafts: Aircraft[];
   ammoStorage: number;
   healthPoint: number;
-  needsAmmo: boolean;
 
-  constructor(ammoStorage, healthPoint){
+  constructor(ammoStorage, healthPoint) {
     this.ammoStorage = ammoStorage;
     this.healthPoint = healthPoint;
     this.aircrafts = [];
   }
 
-  add(ammo, baseDamage, maxAmmo, type, allDamage){
+  add(ammo, baseDamage, maxAmmo, type, allDamage) {
     this.aircrafts.push(new Aircraft(ammo, baseDamage, maxAmmo, type, allDamage));
   }
 
-  fill(){
-    let dividedAmount: number = this.ammoStorage / this.aircrafts.length;
-    this.aircrafts.forEach( element => {
-      element.refill(dividedAmount);
-    });
+  fill() {
+    if(this.ammoStorage != 0){
+      for(let index = 0; index < this.aircrafts.length; index++){
+        if ( this.aircrafts[index].isPriority() === true && this.ammoStorage != 0) {
+          let dividedAmount: number = this.ammoStorage / this.aircrafts.length;
+          this.aircrafts.forEach(element => {
+            element.refill(dividedAmount);
+          });
+        }
+      }
+      for(let index = 0; index < this.aircrafts.length; index++){
+        if (this.ammoStorage != 0) {
+          let dividedAmount: number = this.ammoStorage / this.aircrafts.length;
+          this.aircrafts.forEach(element => {
+            element.refill(dividedAmount);
+          });
+        }
+      }
+    } else {
+      'Ammo storage is empty.';
+    }  
   }
 }
