7309037e3bcc7cd5ac56fa1f5a804bf6ad2ee545
Jenny <lottj440@gmail.com>
2018-09-17 14:45:48 +0200

    todos done

--- week-06/day-04/todos/index.js
+++ week-06/day-04/todos/index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3005;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const todos = [
   'get up',
   'survive',
   'go back to bed',
 ];
 
-app.set('view engine', 'ejs');
+// home page
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Things to do today:',
+    todos: todos,
+  });
+});
 
-app.get('/', (req, res => {
-  for(let index = 0; index < todos.length; index++){
-    
-  }
-}));
-
+// start express app on port 3000
 app.listen(PORT, () => {
-  console.log(`Server running port: ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
