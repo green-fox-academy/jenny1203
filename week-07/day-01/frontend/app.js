ad202d9c8330d253a33157fefc00eb1fdbbc0cba
Jenny <lottj440@gmail.com>
2018-09-30 21:39:27 +0200

    working on append a

--- week-07/day-01/frontend/app.js
+++ week-07/day-01/frontend/app.js
@@ -30,6 +30,35 @@ app.get('/doubling', (req, res) => { //url: localhost:3010/doubling?input=valami
 
 });
 
+app.get('/greeter', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  if (req.query.name && req.query.title !== undefined) {
+    let name = {
+      "welcome_message": "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!"
+    }
+    res.send(JSON.stringify(name));
+  } else if(req.query.name === undefined) {
+    let noName = {
+      "error": "Please provide a name!"
+    }
+    res.send(JSON.stringify(noName));
+  } else if(req.query.title === undefined) {
+    let noTitle = {
+      "error": "Please provide a title!"
+    }
+    res.send(JSON.stringify(noTitle));
+  }
+
+});
+
+app.get('/appenda/:appendable', (req, res) => {
+  let tryOut = {
+    'appended': '' + req.params.appendable + 'a'
+  }
+
+  res.send(JSON.stringify(tryOut));
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
